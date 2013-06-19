03056b1832d6c8a0de6a2dad807e2a5fbf8f814a
David Raison <david@raison.lu>
2013-06-19 20:52:31 +0200

    Fixed the Field Sort class where it was previously impossible to set a field value

--- src/Sherlock/components/sorts/Field.php
+++ src/Sherlock/components/sorts/Field.php
@@ -20,20 +20,30 @@ class Field extends components\BaseComponent implements components\SortInterface
 {
     public function __construct($hashMap = null)
     {
-        $this->params['order']           = null;
+        $this->params['order']           = 'asc';
         $this->params['sort_mode']       = null;
-        $this->params['order']           = null;
         $this->params['missing']         = null;
         $this->params['ignore_unmapped'] = null;
 
         parent::__construct($hashMap);
     }
 
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function field($value)
+    {
+        $this->params['field'] = $value;
+        return $this;
+    }
+
 
     public function toArray()
     {
         $ret = array(
-            $this->params['name'] =>
+            $this->params['field'] =>
             array(
                 'sort_mode'       => $this->params["sort_mode"],
                 'order'           => $this->params["order"],
